4c23429dd51e6a0e3d466ceaff7886eb5d75af79
Kent Fredric <kentfredric@gmail.com>
2013-07-23 23:13:52 +1200

    Further remove getFileContents code

--- examples/getParamFromFile.pl
+++ examples/getParamFromFile.pl
@@ -4,7 +4,10 @@ use warnings;
 use strict;
 
 use PortageXS;
+use Path::Tiny qw(path);
 
 my $pxs=PortageXS->new();
 print "CFLAGS are set to: ";
-print join(' ',$pxs->getParamFromFile($pxs->getFileContents('/etc/make.globals').$pxs->getFileContents('/etc/make.conf'),'CFLAGS','lastseen'))."\n";
+my $content = path('/etc/make.globals')->slurp;
+   $content .= path('/etc/make.conf')->slurp;
+print join(' ',$pxs->getParamFromFile($content,'CFLAGS','lastseen'))."\n";
